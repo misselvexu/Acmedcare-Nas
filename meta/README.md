ac74aefee10a3b752f205120d1606647f417e595
misselvexu <iskp.me@gmail.com>
2019-01-21 13:48:47 +0800

    ADD META SERVER MODULE

---
+++ meta/README.md
@@ -0,0 +1,2 @@
+## Nas Meta Server
+`Nas Meta Server` storage nas file system processed file record , file volume detail info.
---
+++ meta/src/main/java/com/acmedcare/nas/meta/MetaServer.java
@@ -0,0 +1,18 @@
+package com.acmedcare.nas.meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * MetaServer
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2019-01-21.
+ */
+@SpringBootApplication
+public class MetaServer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MetaServer.class, args);
+  }
+}
